97b3401b6c00d35cda4845667e547589c38ad4bf
Mackenzie Stack <stac0092@algonquinlive.com>
2019-10-31 18:43:03 -0400

    4,5,6

--- robots/robot.test.js
+++ robots/robot.test.js
@@ -16,13 +16,13 @@ describe('Robot module', () => {
     // BUILD YOUR TESTS HERE
     
     // 1. Test that result is an array.
-    test('array test', () => {
+    test('result is an array', () => {
         expect(Array.isArray(['result'])).toBe(true);
       })
 
     // 2. Test that result[1] is of type object.
 
-    test('result object test', () => {
+    test('result is of type object', () => {
         expect(typeof result[1] === "object").toBe(true); 
       })
 
@@ -32,15 +32,28 @@ describe('Robot module', () => {
         const report = result[1]; 
 
         // 3. Test that report contains the properties: action, x, y, and facing.
+          test('report properties include action, x, y, and facing', () => {
+            expect(report).toHaveProperty('action','x','y','facing')
+          });
 
 
         // 4. Test that report.x is a number between 0 and 5.
-
-
+        test('report x between 0 and 5', () => {
+            expect(report.x).toBeGreaterThanOrEqual(0);
+            expect(report.x).toBeLessThan(5);
+          });
         // 5. Test that report.y is a number between 0 and 5.
-
+        test('report y between 0 and 5', () => {
+            expect(report.y).toBeGreaterThanOrEqual(0);
+            expect(report.y).toBeLessThan(5);
+          });
 
         // 6. Test that report.facing is one of: NORTH, SOUTH, EAST, or WEST.
+
+        //doesn't feel right lol but it was a simple solution
+        test('facing includes one of NORTH, SOUTH, EAST, WEST', () => {
+            expect(report.facing === "NORTH" || report.facing === "SOUTH" || report.facingr === "EAST" || report.facing === "WEST").toBeTruthy();
+          });
 
     })
 })
